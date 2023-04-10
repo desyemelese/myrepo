7271b50957af7b813b599b9ff3576223e8d15e56
desyemelese <melesedes23@gmail.com>
2023-04-10 05:34:04 -0700

    first commit

---
+++ gitint.py
@@ -0,0 +1,52 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[ ]:
+
+
+# Enter two numbers 
+n1 = input('Enter number 1: ')
+n2 = input('Enter number 2: ')
+
+# Add them together
+sum = float(n1) + float(n2)
+
+# Display the result
+print('The sum of {0} and {1} is {2}'.format(n1, n2, sum))
+
+
+# In[ ]:
+
+
+
+
+
+# In[ ]:
+
+
+
+
+
+# In[ ]:
+
+
+
+
+
+# In[ ]:
+
+
+
+
+
+# In[ ]:
+
+
+
+
+
+# In[ ]:
+
+
+
+
